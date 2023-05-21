d487991f668872b281c283ed0d386a2a95e0528f
Geo Grigor <geogrigor@gmail.com>
2023-05-21 16:40:48 +0200

    ✨ add checkout command

--- githancer/githancer.py
+++ githancer/githancer.py
@@ -1,6 +1,7 @@
 import argparse
 import subprocess
 
+
 def run_git_command(command):
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
@@ -12,7 +13,28 @@ def run_git_command(command):
 
 
 def main():
-   print('Bleh')
+    parser = argparse.ArgumentParser(description='Run git commands')
+    parser.add_argument('command', type=str)
+    parser.add_argument('-b', '--branch', type=str, help='The branch to checkout')
+
+    args = parser.parse_args()
+
+    if args.command == '' or args.command == None:
+        print('No command specified')
+        return
+
+    if args.command == 'checkout':
+        if args.branch == '' or args.branch == None:
+            print('No branch specified')
+            return
+
+        git_command = f'git checkout {args.branch}'
+        output = run_git_command(git_command)
+
+        print(output)
+    else:
+        print('Invalid command')
+        return
 
 
 if __name__ == "__main__":
