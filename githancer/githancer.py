bc3a2b75ecfa81b030a827a665e12df8da272f69
Geo Grigor <geogrigor@gmail.com>
2023-05-21 17:05:45 +0200

    ✨ add auto stash apply functionality

--- githancer/githancer.py
+++ githancer/githancer.py
@@ -8,16 +8,42 @@ def run_git_command(command):
     output, error = process.communicate()
 
     if error:
-        raise Exception("Error occurred while executing command: ", error)
+        raise Exception('Error occurred while executing command: ', error)
     else:
-        return output.decode("utf-8").strip()
+        return output.decode('utf-8').strip()
 
 
 def auto_stash():
-    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    current_time = datetime.datetime.now().strftime('%H:%M:%S')
     current_branch = run_git_command('git rev-parse --abbrev-ref HEAD')
     stash_name = f'githancer-auto-stash {current_branch} {current_time}'
     git_command = f'git stash save {stash_name}'
+    output = run_git_command(git_command)
+
+    print(output)
+
+
+def apply_branch_stash(branch):
+    stashes = run_git_command('git stash list')
+    stash_startswith = f'githancer-auto-stash {branch}'
+
+    for stash in stashes.split('\n'):
+        if stash.startswith(stash_startswith):
+            target_stash = stash.split(':')[0]
+            break
+
+    if target_stash is None:
+        return
+
+    stash_output = run_git_command('git stash apply {}'.format(target_stash))
+    drop_output = run_git_command('git stash drop {}'.format(target_stash))
+
+    print(stash_output)
+    print(drop_output)
+
+
+def checkout_branch(branch):
+    git_command = f'git checkout {branch}'
     output = run_git_command(git_command)
 
     print(output)
@@ -29,11 +55,8 @@ def stash_checkout(branch):
         return
 
     auto_stash()
-
-    git_command = f'git checkout {branch}'
-    output = run_git_command(git_command)
-
-    print(output)
+    checkout_branch(branch)
+    apply_branch_stash(branch)
 
 
 def main():
@@ -56,5 +79,5 @@ def main():
         return
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
